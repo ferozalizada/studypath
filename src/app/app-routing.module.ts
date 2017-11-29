184ef88e80a78e0286123ed7ca9f7ad74fd363b5
Feroz Alizada <ferozalizada@Ferozs-MacBook-Air.local>
2017-11-28 22:39:34 -0500

    Added Jquery & Bootstrap Dependecies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
+//imports the modules
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//Imports all the components 
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { SemesterCourseViewComponent } from './components/semester-course-view/semester-course-view.component';
+import { SemesterDropdownComponent } from './components/semester-dropdown/semester-dropdown.component';
+import { SemesterViewComponent } from './components/semester-view/semester-view.component';
+
+//Adds the routes the components and provides the http path for them 
+const routes: Routes = [
+  {
+    path : 'course-list',
+    component: CourseListComponent,
+    outlet: 'aside'
+  },
+  {
+    path: 'calendar',
+    component: CalendarComponent
+  },
+  {
+    path: 'semester-course-view',
+    component: SemesterCourseViewComponent
+  },
+  {
+    path: 'semester-view',
+    component: SemesterViewComponent
+  },
+  {
+    path: 'semester-dropdown',
+    component: SemesterDropdownComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
+//Adds all the modules required for angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
+//Imports all the required services
+import { CourseDataService } from './services/course-data.service';
+import { SemesterListService } from './services/semester-list.service';
+import { ApiRequestsService } from './services/api-requests.service';
+
+//Imports the required components
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { CourseListComponent } from './components/course-list/course-list.component';
-import { CourseDataService } from './services/course-data.service';
-import { SemesterDropdownComponent } from './components/semester-dropdown/semester-dropdown.component';
-import { SemesterListService } from './services/semester-list.service';
 import { SemesterViewComponent } from './components/semester-view/semester-view.component';
 import { SemesterCourseViewComponent } from './components/semester-course-view/semester-course-view.component';
-import { ApiRequestsService } from './services/api-requests.service';
+import { SemesterDropdownComponent } from './components/semester-dropdown/semester-dropdown.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
 
 @NgModule({
   declarations: [
@@ -27,7 +35,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [
     CourseDataService,
