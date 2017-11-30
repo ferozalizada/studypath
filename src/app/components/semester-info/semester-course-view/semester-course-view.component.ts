d4c9abde92929e1da170673494a71707d78a7887
Feroz Alizada <ferozalizada@Ferozs-MacBook-Air.local>
2017-11-30 18:37:32 -0500

    Added cards and auto-generation for semester-view

--- src/app/components/semester-info/semester-course-view/semester-course-view.component.ts
+++ src/app/components/semester-info/semester-course-view/semester-course-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../../../classes/course';
+import { CourseDataService } from '../../../services/course-data.service';
 
 @Component({
   selector: 'app-semester-course-view',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./semester-course-view.component.css']
 })
 export class SemesterCourseViewComponent implements OnInit {
+  courseList : Course[];
 
-  constructor() { }
+  constructor( private courseDataService: CourseDataService) { 
+    courseDataService.initCoursesArray();
+    this.courseList = courseDataService.getAllCourses();
+  }
 
   ngOnInit() {
   }
