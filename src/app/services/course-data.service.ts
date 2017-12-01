bdab16b040de25cf69b0aab4d8e0bdaadab91dda
Nick Molinari <nmoli074@uottawa.ca>
2017-12-01 17:01:57 -0500

    Course data service populates from database

--- src/app/services/course-data.service.ts
+++ src/app/services/course-data.service.ts
@@ -1,37 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../classes/course';
+import { ApiRequestsService } from './api-requests.service';
 
 
 @Injectable()
 export class CourseDataService {
   private courses:Course[];
-
-  constructor() { 
+  private api: ApiRequestsService;
+  
+  constructor(api: ApiRequestsService) { 
     this.courses = [];
+    this.api = api;
     this.initCoursesArray();
   }
-
-  initCoursesArray(){
-    //temp function ... to be replaced by fetchAllCourses
-    var c1 = new Course();
-
-    c1.setId(10);
-    c1.setCode('B00');
-    c1.setName('Lecture');
-    c1.setDescription('Henderson Residence 013');
-    this.courses.push(c1);
-
-    var c1 = new Course();
-    c1.setCode('B001');
-    c1.setDescription('Tabaret at 13;00');
-    c1.setId(1);
-    c1.setName('ADM1101');
-    this.courses.push(c1);
+  
+  initCoursesArray() {
+    this.api.getAllCourses(this.addCoursesToArray.bind(this));
   }
 
-  fetchAllCourses(){
-    //Later real data pulled from database will be put into courses array
-    //Note: enter manually data in database, both english and french version
+  addCoursesToArray(courses){
+    this.courses = courses;
+    console.log(this.courses);
   }
 
   getAllCourses(){
