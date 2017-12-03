d48a20f85f4d6de657e07c920dfffab3716148ff
Nick Molinari <nmoli074@uottawa.ca>
2017-12-02 22:39:01 -0500

    Added filters to student creation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -21,6 +21,7 @@ export class LoginComponent implements OnInit {
     private userAuth: UserAuthenticationService,
     private modalService: NgbModal, 
     private apiRequestsService: ApiRequestsService) { }
+    private newStudent: Student;
 
   ngOnInit() {
     
@@ -52,20 +53,40 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  addUserIfUnique (stu) {
+    if (stu != undefined) {
+      this.apiRequestsService.addStudent(this.newStudent);
+      alert("Your account has been created");
+      //document.getElementById('exampleModel').modal('hide');
+    } else {
+      alert("A student with this ID already exists!");
+    }
+  }
+
   registerUser(e){
     //registers the user
     this.studentID = e.target.elements[0].value;
     var firstName = e.target.elements[1].value;
     var lastName = e.target.elements[2].value;
     var startYear = e.target.elements[3].value;
-    var endYear = (Number.parseInt(startYear) + 4).toString();
-    console.log("Your student id  :" + this.studentID);
+    if (Number(startYear) > 2000 && Number(startYear) < 2099) {
+      var endYear = (Number.parseInt(startYear) + 4).toString();
 
-    var newStudent = new Student(Number.parseInt(this.studentID), firstName, lastName);
-    newStudent.setStartYear(startYear);
-    newStudent.setEndYear(endYear);
-    newStudent.addSemester("Fall 2017");
-    this.apiRequestsService.addStudent(newStudent);
-    this.router.navigate(['/scheduler']);
+      if (this.studentID == ""  || firstName == "" || lastName == ""  || endYear == ""   || startYear == "") {
+        alert("Please fill out all fields!");
+      } else if (Number(this.studentID) > 0) {
+        this.newStudent = new Student(Number.parseInt(this.studentID), firstName, lastName);
+        this.newStudent.setStartYear(startYear);
+        this.newStudent.setEndYear(endYear);
+        this.newStudent.addSemester("Fall 2017");
+        this.apiRequestsService.getStudentById(this.addUserIfUnique.bind(this),this.studentID);
+      } else {
+        alert("Student ID can only contain numbers!");
+      }
+    } else {
+      alert ("Year must be a number in this century!");
+    }
+    }
   }
+  
 }
